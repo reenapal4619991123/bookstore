f1c1c622fdfc9587f794f6dcbc34fdcd406ac33c
reenapal4619991123 <120025778+reenapal4619991123@users.noreply.github.com>
2023-02-17 11:06:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap .from(".col-3",{
+  scale:0,
+  opacity:0,
+  duration:5,
+  color:"red"
+})
+gsap .from(".col-4",{
+  scale:0,
+  opacity:0,
+  duration:10,
+  color:"red"
+})
+gsap .from(".offer",{
+scale:0,
+opacity:0,
+duration:13,
+color:"#fff"
+
+})
